20d414d2cac126a48dd3144c618710687cf6d247
Guscraftin <97472565+Guscraftin@users.noreply.github.com>
2023-05-10 20:34:38 +0100

    update: GatewayIntent for logs

--- index.js
+++ index.js
@@ -2,7 +2,27 @@ const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [
+    GatewayIntentBits.AutoModerationConfiguration, // For the future
+    GatewayIntentBits.AutoModerationExecution, // For the future
+    GatewayIntentBits.DirectMessageReactions, // For the future
+    GatewayIntentBits.DirectMessageTyping, // For the future
+    GatewayIntentBits.DirectMessages, // For the future
+    GatewayIntentBits.GuildEmojisAndStickers, // For the future
+    GatewayIntentBits.GuildIntegrations, // For the future
+    GatewayIntentBits.GuildInvites, // For the future
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessageReactions, // For the future
+    GatewayIntentBits.GuildMessageTyping, // For the future
+    GatewayIntentBits.GuildMessages, 
+    GatewayIntentBits.GuildModeration, // For the future
+    GatewayIntentBits.GuildPresences, // For the future
+    GatewayIntentBits.GuildScheduledEvents, // For the future
+    GatewayIntentBits.GuildVoiceStates, // For the future
+    GatewayIntentBits.GuildWebhooks, // For the future
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent,
+]});
 client.commands = new Collection();
 client.buttons = new Collection();
 client.selectMenus = new Collection();
