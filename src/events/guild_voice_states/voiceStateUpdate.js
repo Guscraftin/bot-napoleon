4c6c711cf5a4e10b8046e0ca30cc80c3c4627c55
Guscraftin <97472565+Guscraftin@users.noreply.github.com>
2023-05-29 23:01:32 +0100

    feature: update automatic auto-vocal

--- src/events/guild_voice_states/voiceStateUpdate.js
+++ src/events/guild_voice_states/voiceStateUpdate.js
@@ -1,12 +1,15 @@
-const { Events, EmbedBuilder } = require('discord.js');
+const { ChannelType, Events, EmbedBuilder, PermissionFlagsBits } = require('discord.js');
+const { Vocals } = require('../../dbObjects');
 const { getLogChannel } = require('../../functions');
 
 module.exports = {
-    name: Events.GuildVoiceStatesUpdate,
+    name: Events.VoiceStateUpdate,
     async execute(oldState, newState){
+        /*
+         * Logs
+         */
         const logChannel = await getLogChannel(oldState.guild, 'logs_generals');
-        if (!logChannel) return;
-
+        if (logChannel) {
         if (oldState.channel === null) {
             const embedJoin = new EmbedBuilder()
                 .setAuthor({ name: newState.member.user.tag, iconURL: newState.member.displayAvatarURL() })
@@ -52,7 +55,69 @@ module.exports = {
                 .setFooter({ text: oldState.guild.name, iconURL: oldState.guild.iconURL() })
 
             logChannel.send({ embeds: [embedDeaf] });
+        }}
 
-        } 
+
+        /*
+         * Auto-Vocals
+         */
+        const oldChannel = oldState.channel;
+        const newChannel = newState.channel;
+        let old_vocal = null;
+        let new_vocal = null;
+        if (oldChannel) old_vocal = await Vocals.findOne({ where: { voice_id: oldChannel.id } });
+        if (newChannel) new_vocal = await Vocals.findOne({ where: { voice_id: newChannel.id } });
+
+        // If the new channel is a auto-vocal
+        if (new_vocal) {
+            await newChannel.clone({
+                name: `${newState.member.user.username}`,
+            }).then(async (channel) => {
+                // Move the user to the new channel
+                await newState.setChannel(channel);
+
+                // Overwrite the permissions
+                await channel.permissionOverwrites.edit(newState.member.id, {
+                    ViewChannel: true,
+                    ManageChannels: true,
+                    ManageRoles: true,
+                    CreateInstantInvite: true,
+                    Connect: true,
+                    PrioritySpeaker: true,
+                });
+            });
+
+        }
+
+        // If the old channel is empty and it's not a auto-vocal, delete it
+        if (oldChannel) {
+            if (!newChannel || oldChannel.id !== newChannel.id) {
+                
+                // If the channel category has a auto-vocal
+                const category = oldChannel.parent;
+                if (category) {
+                    const category_vocals = await Vocals.findAll({ where: { category_id: category.id } });
+                    if (category_vocals.length > 0) {
+
+                        // If the channel are not ignore by the auto-vocal
+                        let ignore = false;
+                        for (const vocal of category_vocals) {
+                            const ignore_voices = vocal.ignore_voices;
+                            for (const ignore_voice of ignore_voices) {
+                                if (ignore_voice === oldChannel.id) ignore = true;
+                            }
+                            if (vocal.voice_id === oldChannel.id) ignore = true;
+                        }
+
+                        if (!ignore) {
+                            // If the channel not a auto-vocal and is empty, delete it
+                            if (!old_vocal && oldChannel.members.size === 0) {
+                                await oldChannel.delete();
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 };
