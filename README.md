55f2d9778221debe8a6b236629757797475cba27
Guscraftin <97472565+Guscraftin@users.noreply.github.com>
2023-05-30 21:10:03 +0100

    update: sync role - add leaders, topstaff, staff

--- README.md
+++ README.md
@@ -2,12 +2,37 @@
 
 Voici la structure pour créer un bot général.
 
-## Installation du bot
+## Setup du bot
+### Installation du bot
 
 - Clone ce répertoire (avoir git installé)
 - Remplir le fichier `.env` avec vos valeurs
 - Exécuter la commmande `npm i` à la racine du dossier précédemment clone (avoir nodejs installé)
 
-## Lancement du bot
+### Lancement du bot
 
 - Exécuter la commmande `node .` dans le dossier précédemment clone
+
+
+
+## ToDo:
+
+[x] Put in one line the error where: Unknown interaction 
+[ ] Fix Unknown interaction when person interact for the first time (with an old button ?)
+
+[ ] Add all logs [EN COURS]
+[ ] Add automatically roles when enter ou leave the staff
+[ ] Deploy commands only on 3 servers except for /infos (create the command and invite people who are not already on the main server ? really util ? it is consider as spam people MP who can report the bot ? Warning 100 servers, lose permissions; two bots ?)
+
+
+### Commands
+- dbinitlogs: create the line for a guild in logs db (admin only)
+- dbconfig: update the database (admin only) 
+- dbvocals: different commands to control vocals db (admin only)
+- docs: send information message (admin only)
+- info: information about the bot
+- syncroles: force syncro roles between discord servers (admin only)
+
+### Features
+- Logs in several channels according of the type (use database)
+- Syncro roles between discord servers all day at 5am (verify) (fr/en, dirigeants en/fr, staff, haut staff)
--- src/commands/admin/syncroles.js
+++ src/commands/admin/syncroles.js
@@ -8,18 +8,30 @@ module.exports = {
 		.setDMPermission(false),
 	async execute(interaction) {
         await interaction.deferReply({ ephemeral: true });
-    
+
         // Sync roles
         if (!await syncRoles(interaction.client)) {
-            return await interaction.editReply({ content: "Une erreur est survenue lors de la synchronisation des rôles.", ephemeral: true });
+            return interaction.editReply({ content: "Une erreur est survenue lors de la synchronisation des rôles.", ephemeral: true });
         } else {
-            await interaction.editReply({ content: "Les rôles ont été synchronisés.", ephemeral: true });
+            return interaction.editReply({ content: "Les rôles ont été synchronisés.", ephemeral: true });
         }
 	}, syncRoles
 };
 
 async function syncRoles(client) {
-    const { role_fr_french, role_en_english, role_main_french, role_main_english } = require('../../const.json');
+    const { 
+        role_fr_french, role_en_english, role_main_french, role_main_english,
+        role_fr_leaders, role_en_leaders, role_main_fr_leaders, role_main_en_leaders,
+        role_fr_staff, role_en_staff, role_main_staff, role_main_founders,
+        role_fr_topstaff, role_en_topstaff, role_main_topstaff
+    } = require('../../const.json');
+
+    /*
+     * Sync roles
+     * - french and english roles
+     * - french and english leader roles
+     * - staff roles 
+     */
 
     // Get guilds
     const guild_main = await client.guilds.fetch(process.env.GUILD_ID_MAIN);
@@ -31,29 +43,55 @@ async function syncRoles(client) {
     // Get roles in nations guilds
     const roles_fr = await guild_fr.roles.fetch(role_fr_french);
     const roles_en = await guild_en.roles.fetch(role_en_english);
+    const roles_fr_leaders = await guild_fr.roles.fetch(role_fr_leaders);
+    const roles_en_leaders = await guild_en.roles.fetch(role_en_leaders);
+    const roles_fr_staff = await guild_fr.roles.fetch(role_fr_staff);
+    const roles_en_staff = await guild_en.roles.fetch(role_en_staff);
+    const roles_fr_topstaff = await guild_fr.roles.fetch(role_fr_topstaff);
+    const roles_en_topstaff = await guild_en.roles.fetch(role_en_topstaff);
 
     const members_fr = roles_fr.members;
     const members_en = roles_en.members;
+    const members_fr_leaders = roles_fr_leaders.members;
+    const members_en_leaders = roles_en_leaders.members;
+    const members_fr_staff = roles_fr_staff.members;
+    const members_en_staff = roles_en_staff.members;
+    const members_fr_topstaff = roles_fr_topstaff.members;
+    const members_en_topstaff = roles_en_topstaff.members;
 
     // Get roles in main guild
     const roles_main_fr = await guild_main.roles.fetch(role_main_french);
     const roles_main_en = await guild_main.roles.fetch(role_main_english);
+    const roles_main_fr_leaders = await guild_main.roles.fetch(role_main_fr_leaders);
+    const roles_main_en_leaders = await guild_main.roles.fetch(role_main_en_leaders);
+    const roles_main_staff = await guild_main.roles.fetch(role_main_staff);
+    const roles_main_founders = await guild_main.roles.fetch(role_main_founders);
+    const roles_main_topstaff = await guild_main.roles.fetch(role_main_topstaff);
 
     const members_main_fr = roles_main_fr.members;
     const members_main_en = roles_main_en.members;
+    const members_main_fr_leaders = roles_main_fr_leaders.members;
+    const members_main_en_leaders = roles_main_en_leaders.members;
+    const members_main_staff = roles_main_staff.members.concat(roles_main_founders.members);
+    const members_main_topstaff = roles_main_topstaff.members;
+
 
     // Sync roles
     await members_fr.forEach(async member => {
         if (!members_main_fr.has(member.id)) {
-            const guildMember = await guild_main.members.fetch(member.id);
-            await guildMember.roles.add(role_main_french);
+            try {
+                const guildMember = await guild_main.members.fetch(member.id);
+                if (guildMember) await guildMember.roles.add(role_main_french);
+            } catch (error) {}
         }
     });
 
     await members_en.forEach(async member => {
         if (!members_main_en.has(member.id)) {
-            const guildMember = await guild_main.members.fetch(member.id);
-            await guildMember.roles.add(role_main_english);
+            try {
+                const guildMember = await guild_main.members.fetch(member.id);
+                if (guildMember) await guildMember.roles.add(role_main_english);
+            } catch (error) {}
         }
     });
 
@@ -66,6 +104,96 @@ async function syncRoles(client) {
     await members_main_en.forEach(async member => {
         if (!members_en.has(member.id)) {
             await member.roles.remove(role_main_english);
+        }
+    });
+
+    await members_fr_leaders.forEach(async member => {
+        if (!members_main_fr_leaders.has(member.id)) {
+            try {
+                const guildMember = await guild_main.members.fetch(member.id);
+                if (guildMember) await guildMember.roles.add(role_main_fr_leaders);
+            } catch (error) {}
+        }
+    });
+
+    await members_en_leaders.forEach(async member => {
+        if (!members_main_en_leaders.has(member.id)) {
+            try {
+                const guildMember = await guild_main.members.fetch(member.id);
+                if (guildMember) await guildMember.roles.add(role_main_en_leaders);
+            } catch (error) {}
+        }
+    });
+
+    await members_main_fr_leaders.forEach(async member => {
+        if (!members_fr_leaders.has(member.id)) {
+            await member.roles.remove(role_main_fr_leaders);
+        }
+    });
+
+    await members_main_en_leaders.forEach(async member => {
+        if (!members_en_leaders.has(member.id)) {
+            await member.roles.remove(role_main_en_leaders);
+        }
+    });
+
+    await members_main_staff.forEach(async member => {
+        if (!members_fr_staff.has(member.id)) {
+            try {
+                const guildMember = await guild_fr.members.fetch(member.id);
+                if (guildMember) await guildMember.roles.add(role_fr_staff);
+            } catch (error) {}
+        }
+    });
+
+    await members_main_staff.forEach(async member => {
+        if (!members_en_staff.has(member.id)) {
+            try {
+                const guildMember = await guild_en.members.fetch(member.id);
+                if (guildMember) await guildMember.roles.add(role_en_staff);
+            } catch (error) {}
+        }
+    });
+
+    await members_fr_staff.forEach(async member => {
+        if (!members_main_staff.has(member.id)) {
+            await member.roles.remove(role_fr_staff);
+        }
+    });
+
+    await members_en_staff.forEach(async member => {
+        if (!members_main_staff.has(member.id)) {
+            await member.roles.remove(role_en_staff);
+        }
+    });
+
+    await members_main_topstaff.forEach(async member => {
+        if (!members_fr_topstaff.has(member.id)) {
+            try {
+                const guildMember = await guild_fr.members.fetch(member.id);
+                if (guildMember) await guildMember.roles.add(role_fr_topstaff);
+            } catch (error) {}
+        }
+    });
+
+    await members_main_topstaff.forEach(async member => {
+        if (!members_en_topstaff.has(member.id)) {
+            try {
+                const guildMember = await guild_en.members.fetch(member.id);
+                if (guildMember) await guildMember.roles.add(role_en_topstaff);
+            } catch (error) {}
+        }
+    });
+
+    await members_fr_topstaff.forEach(async member => {
+        if (!members_main_topstaff.has(member.id)) {
+            await member.roles.remove(role_fr_topstaff);
+        }
+    });
+
+    await members_en_topstaff.forEach(async member => {
+        if (!members_main_topstaff.has(member.id)) {
+            await member.roles.remove(role_en_topstaff);
         }
     });
 
