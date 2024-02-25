3901a5c747905cec280aad0777ee554a0da4a47e
Guscraftin <97472565+Guscraftin@users.noreply.github.com>
2024-02-25 14:47:34 +0100

    fix(guildMemberRemove): remove kicklog because of undefined member

--- src/events/guild_members/guildMemberRemove.js
+++ src/events/guild_members/guildMemberRemove.js
@@ -7,17 +7,6 @@ module.exports = {
         const logChannel = await getLogChannel(member.guild, 'logs_members');
         if (!logChannel) return;
 
-        const fetchKickLog = await member.guild.fetchAuditLogs({
-            limit: 1,
-            type: 20
-        });
-
-        const kickLog = fetchKickLog.entries.first();
-        const { target, reason } = kickLog;
-        let isMemberKick = false;
-
-        if (target.id === member.id) isMemberKick = true;
-
         const embed = new EmbedBuilder()
             .setAuthor({ name: `${member.user.tag} (${member.id})`, iconURL: member.user.displayAvatarURL() })
             .setColor('#dc143c')
@@ -25,7 +14,6 @@ module.exports = {
             • Créé le : <t:${parseInt(member.user.createdTimestamp / 1000)}:f> (<t:${parseInt(member.user.createdTimestamp / 1000)}:R>)
             • Rejoint le : <t:${parseInt(member.joinedTimestamp / 1000)}:f> (<t:${parseInt(member.joinedTimestamp / 1000)}:R>)
             • Quitté le : <t:${parseInt(Date.now() / 1000)}:f> (<t:${parseInt(Date.now() / 1000)}:R>)
-            • Kick ? : ${isMemberKick ? `🟢 (raison : ${reason})` : `🔴`}
             `)
             .setTimestamp()
             .setFooter({ text: "L'utilisateur a quitté !" })
